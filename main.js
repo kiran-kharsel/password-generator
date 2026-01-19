c5ae3782812c937dc6c0c8f2e38f4ace0ce74712
kiran-kharsel <kirankharsel@gmail.com>
2026-01-19 10:14:21 +0530

    feat: create uppercase password

---
+++ main.js
@@ -0,0 +1,66 @@
+// dom elem
+
+const generatePasswordBtn = document.querySelector('.generate-password');
+const charRangeInput = document.querySelector('#range')
+const charLengthElem = document.querySelector('.char-length')
+
+// checkboxes
+const allCheckBoxes = document.querySelectorAll('.checkbox-container input')
+console.log(allCheckBoxes)
+
+
+// min character length is 5
+charLengthElem.innerText = charRangeInput.value;
+
+
+
+// password and characters array
+let passwordCharacters = '';
+let uppercaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let lowercaseChar = 'abcdefghijklmnopqrstuvwxyz';
+let numbers = '0123456789';
+let symbol = '~!#$%^&*()-+';
+
+// show character length when range change
+charRangeInput.addEventListener('input', function(){
+    console.log('range')
+    charLengthElem.innerText = charRangeInput.value
+});
+
+allCheckBoxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', function(){
+        console.log(checkbox.id);
+        if(checkbox.id === 'uppercase' && checkbox.checked){
+            // passwordCharacters.push(uppercaseChar)
+            passwordCharacters += uppercaseChar;
+            
+        }
+
+        if(checkbox.id === 'lowercase' && checkbox.checked){
+            passwordCharacters.push(lowercaseChar)
+        }
+
+        if(checkbox.id === 'numbers' && checkbox.checked){
+            passwordCharacters.push(numbers)
+        }
+
+        if(checkbox.id === 'symbol' && checkbox.checked){
+            passwordCharacters.push(symbol)
+        }
+        //console.log(passwordCharacters)
+    })
+})
+
+
+generatePasswordBtn.addEventListener('click', function(){
+    console.log(charRangeInput.value)
+    let charRange = charRangeInput.value;
+    console.log(passwordCharacters.length)
+
+    // loop upto range to get password
+    for(let i=1; i <= charRange; i++){
+        let randomNum = Math.floor(Math.random() * passwordCharacters.length);
+        console.log(passwordCharacters[randomNum])
+    }
+});
+
