4084445be76ce545c300b22e9989989f84e1aef4
kiran-kharsel <kirankharsel@gmail.com>
2026-01-19 11:53:08 +0530

    remove console log

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const generatePasswordBtn = document.querySelector(".generate-password");
 const charRangeInput = document.querySelector("#range");
 const charLengthElem = document.querySelector(".char-length");
 const passwordElem = document.querySelector(".password");
+const copyPasswordBtn = document.querySelector(".copy-btn");
 
 // checkboxes
 const allCheckBoxes = document.querySelectorAll(".checkbox-container input");
-console.log(allCheckBoxes);
 
 // min character length is 5
 charLengthElem.innerText = charRangeInput.value;
@@ -21,52 +21,53 @@ let symbol = "~!#$%^&*()-+";
 
 // show character length when range change
 charRangeInput.addEventListener("input", function () {
-  console.log("range");
   charLengthElem.innerText = charRangeInput.value;
 });
 
+allCheckBoxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", function () {
+    console.log(checkbox);
+    //reset
 
+    if (checkbox.id === "uppercase" && checkbox.checked) {
+      passwordCharacters += uppercaseChar;
+    } else if (checkbox.id === "uppercase" && !checkbox.checked) {
+      passwordCharacters = passwordCharacters.replace(uppercaseChar, "");
+    }
 
- allCheckBoxes.forEach((checkbox) => {
-    checkbox.addEventListener("change", function () {
-      console.log(checkbox);
-      //reset
+    if (checkbox.id === "lowercase" && checkbox.checked) {
+      passwordCharacters += lowercaseChar;
+    } else if (checkbox.id === "lowercase" && !checkbox.checked) {
+      passwordCharacters = passwordCharacters.replace(lowercaseChar, "");
+    }
 
-      if (checkbox.id === "uppercase" && checkbox.checked) {
-        passwordCharacters += uppercaseChar;
-      } else if(checkbox.id === "uppercase" && !checkbox.checked){
-        passwordCharacters = passwordCharacters.replace(uppercaseChar, '')
-      }
+    if (checkbox.id === "numbers" && checkbox.checked) {
+      passwordCharacters += numbers;
+    } else if (checkbox.id === "numbers" && !checkbox.checked) {
+      passwordCharacters = passwordCharacters.replace(numbers, "");
+    }
 
-      if (checkbox.id === "lowercase" && checkbox.checked) {
-        passwordCharacters += lowercaseChar;
-      }else if(checkbox.id === "lowercase" && !checkbox.checked){
-        passwordCharacters = passwordCharacters.replace(lowercaseChar, '')
-      }
-
-      if (checkbox.id === "numbers" && checkbox.checked) {
-        passwordCharacters += numbers;
-      }else if(checkbox.id === "numbers" && !checkbox.checked){
-        passwordCharacters = passwordCharacters.replace(numbers, '')
-      }
-
-      if (checkbox.id === "symbol" && checkbox.checked) {
-        passwordCharacters += symbol;
-      }else if(checkbox.id === "symbol" && !checkbox.checked){
-        passwordCharacters = passwordCharacters.replace(symbol, '')
-      }
-      
-    });
-
+    if (checkbox.id === "symbol" && checkbox.checked) {
+      passwordCharacters += symbol;
+    } else if (checkbox.id === "symbol" && !checkbox.checked) {
+      passwordCharacters = passwordCharacters.replace(symbol, "");
+    }
   });
-
+});
 
 generatePasswordBtn.addEventListener("click", function () {
-  console.log(passwordCharacters);
+  //check if no checkbox is checked
+  if (passwordCharacters === "" || passwordCharacters === undefined) {
+    // copy fn btn disabled
+    copyPasswordBtn.disabled = true;
+    passwordElem.innerText = "click on checkbox";
+    return;
+  }
+
   let charRange = charRangeInput.value;
   let password = "";
-
-  
+  // copy fn btn enable
+  copyPasswordBtn.disabled = false;
 
   // loop upto range to get password
   for (let i = 1; i <= charRange; i++) {
@@ -74,6 +75,11 @@ generatePasswordBtn.addEventListener("click", function () {
     password += passwordCharacters[randomNum];
   }
 
-  console.log(password);
   passwordElem.innerText = password;
+});
+
+// copy password
+copyPasswordBtn.addEventListener("click", function () {
+    console.log('copied')
+  navigator.clipboard.writeText(passwordElem.innerText);
 });
