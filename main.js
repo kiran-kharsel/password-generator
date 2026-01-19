9e3bcc73eaaf97c1094de70f8fe085d025090ccc
kiran-kharsel <kirankharsel@gmail.com>
2026-01-19 17:02:02 +0530

    refactor: copy only when password generate

--- main.js
+++ main.js
@@ -6,58 +6,24 @@ const charLengthElem = document.querySelector(".char-length");
 const passwordElem = document.querySelector(".password");
 const copyPasswordBtn = document.querySelector(".copy-btn");
 
-// checkboxes
-const allCheckBoxes = document.querySelectorAll(".checkbox-container input");
+
 
 // min character length is 5
 charLengthElem.innerText = charRangeInput.value;
 
-// password and characters array
-let passwordCharacters = "";
-let uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-let numbers = "0123456789";
-let symbol = "~!#$%^&*()-+";
 
 // show character length when range change
 charRangeInput.addEventListener("input", function () {
   charLengthElem.innerText = charRangeInput.value;
 });
 
-allCheckBoxes.forEach((checkbox) => {
-  checkbox.addEventListener("change", function () {
-    console.log(checkbox);
-    //reset
 
-    if (checkbox.id === "uppercase" && checkbox.checked) {
-      passwordCharacters += uppercaseChar;
-    } else if (checkbox.id === "uppercase" && !checkbox.checked) {
-      passwordCharacters = passwordCharacters.replace(uppercaseChar, "");
-    }
-
-    if (checkbox.id === "lowercase" && checkbox.checked) {
-      passwordCharacters += lowercaseChar;
-    } else if (checkbox.id === "lowercase" && !checkbox.checked) {
-      passwordCharacters = passwordCharacters.replace(lowercaseChar, "");
-    }
-
-    if (checkbox.id === "numbers" && checkbox.checked) {
-      passwordCharacters += numbers;
-    } else if (checkbox.id === "numbers" && !checkbox.checked) {
-      passwordCharacters = passwordCharacters.replace(numbers, "");
-    }
-
-    if (checkbox.id === "symbol" && checkbox.checked) {
-      passwordCharacters += symbol;
-    } else if (checkbox.id === "symbol" && !checkbox.checked) {
-      passwordCharacters = passwordCharacters.replace(symbol, "");
-    }
-  });
-});
 
 generatePasswordBtn.addEventListener("click", function () {
   generatePassword();
 });
+
+
 
 // password generate
 function generatePassword() {
@@ -111,6 +77,8 @@ function generatePassword() {
 
 // copy password
 copyPasswordBtn.addEventListener("click", function () {
-  console.log("copied");
-  navigator.clipboard.writeText(passwordElem.innerText);
+  
+  if(passwordElem.innerText !== 'Password...'){
+    navigator.clipboard.writeText(passwordElem.innerText);
+  }
 });
